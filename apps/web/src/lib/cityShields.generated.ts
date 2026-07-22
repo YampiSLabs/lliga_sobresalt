2c3a68d7d32f7192c4bf6ed1770f3288e17a0dc8
BeatrizAgent <beatrizagent@users.noreply.github.com>
2026-07-22 06:42:49 +0000

    chore: update catalunya shields

--- apps/web/src/lib/cityShields.generated.ts
+++ apps/web/src/lib/cityShields.generated.ts
@@ -104,9 +104,14 @@ import BredaShield from "catalunya-shields/assets/svg/breda.svg";
 import CabanabonaShield from "catalunya-shields/assets/svg/cabanabona.svg";
 import CabanellesShield from "catalunya-shields/assets/svg/cabanelles.svg";
 import CabanesShield from "catalunya-shields/assets/svg/cabanes.svg";
+import CaboShield from "catalunya-shields/assets/svg/cabo.svg";
 import CabraDelCampShield from "catalunya-shields/assets/svg/cabra-del-camp.svg";
 import CabreraDeMarShield from "catalunya-shields/assets/svg/cabrera-de-mar.svg";
 import CabrilsShield from "catalunya-shields/assets/svg/cabrils.svg";
+import CadaquesShield from "catalunya-shields/assets/svg/cadaques.svg";
+import CalafShield from "catalunya-shields/assets/svg/calaf.svg";
+import CalafellShield from "catalunya-shields/assets/svg/calafell.svg";
+import CaldersShield from "catalunya-shields/assets/svg/calders.svg";
 import CornellaDeLlobregatShield from "catalunya-shields/assets/svg/cornella-de-llobregat.svg";
 import LhospitaletDeLlobregatShield from "catalunya-shields/assets/svg/lhospitalet-de-llobregat.svg";
 import MataroShield from "catalunya-shields/assets/svg/mataro.svg";
@@ -235,9 +240,14 @@ export const GENERATED_CITY_SHIELDS: Record<string, ShieldAsset> = {
   "cabanabona": CabanabonaShield,
   "cabanelles": CabanellesShield,
   "cabanes": CabanesShield,
+  "cabo": CaboShield,
   "cabra-del-camp": CabraDelCampShield,
   "cabrera-de-mar": CabreraDeMarShield,
   "cabrils": CabrilsShield,
+  "cadaques": CadaquesShield,
+  "calaf": CalafShield,
+  "calafell": CalafellShield,
+  "calders": CaldersShield,
   "cornella": CornellaDeLlobregatShield,
   "cornella-de-llobregat": CornellaDeLlobregatShield,
   "l-hospitalet-de-llobregat": LhospitaletDeLlobregatShield,
@@ -359,9 +369,14 @@ export const GENERATED_CITY_SHIELD_SLUGS = [
   "cabanabona",
   "cabanelles",
   "cabanes",
+  "cabo",
   "cabra-del-camp",
   "cabrera-de-mar",
   "cabrils",
+  "cadaques",
+  "calaf",
+  "calafell",
+  "calders",
   "cornella-de-llobregat",
   "lhospitalet",
   "mataro",
@@ -479,9 +494,14 @@ export const GENERATED_CITY_SHIELD_CITIES: GeneratedCityShield[] = [
   { name: "Cabanabona", slug: "cabanabona", province: "Lleida", aliases: [], hasShield: true },
   { name: "Cabanelles", slug: "cabanelles", province: "Girona", aliases: [], hasShield: true },
   { name: "Cabanes", slug: "cabanes", province: "Girona", aliases: [], hasShield: true },
+  { name: "Cabó", slug: "cabo", province: "Lleida", aliases: [], hasShield: true },
   { name: "Cabra del Camp", slug: "cabra-del-camp", province: "Tarragona", aliases: [], hasShield: true },
   { name: "Cabrera de Mar", slug: "cabrera-de-mar", province: "Barcelona", aliases: [], hasShield: true },
   { name: "Cabrils", slug: "cabrils", province: "Barcelona", aliases: [], hasShield: true },
+  { name: "Cadaqués", slug: "cadaques", province: "Girona", aliases: [], hasShield: true },
+  { name: "Calaf", slug: "calaf", province: "Barcelona", aliases: [], hasShield: true },
+  { name: "Calafell", slug: "calafell", province: "Tarragona", aliases: [], hasShield: true },
+  { name: "Calders", slug: "calders", province: "Barcelona", aliases: [], hasShield: true },
   { name: "Cornellà de Llobregat", slug: "cornella-de-llobregat", province: "Barcelona", aliases: ["cornella"], hasShield: true },
   { name: "L'Hospitalet de Llobregat", slug: "lhospitalet", province: "Barcelona", aliases: ["l-hospitalet-de-llobregat","lhospitalet-de-llobregat"], hasShield: true },
   { name: "Mataró", slug: "mataro", province: "Barcelona", aliases: [], hasShield: true },
@@ -618,15 +638,15 @@ export const GENERATED_CITIES: GeneratedCityShield[] = [
   { name: "Cabanelles", slug: "cabanelles", province: "Girona", aliases: [], hasShield: true },
   { name: "Cabanes", slug: "cabanes", province: "Girona", aliases: [], hasShield: true },
   { name: "Cabassers", slug: "cabassers", province: "Tarragona", aliases: [], hasShield: false },
-  { name: "Cabó", slug: "cabo", province: "Lleida", aliases: [], hasShield: false },
+  { name: "Cabó", slug: "cabo", province: "Lleida", aliases: [], hasShield: true },
   { name: "Cabra del Camp", slug: "cabra-del-camp", province: "Tarragona", aliases: [], hasShield: true },
   { name: "Cabrera d'Anoia", slug: "cabrera-danoia", province: "Barcelona", aliases: [], hasShield: false },
   { name: "Cabrera de Mar", slug: "cabrera-de-mar", province: "Barcelona", aliases: [], hasShield: true },
   { name: "Cabrils", slug: "cabrils", province: "Barcelona", aliases: [], hasShield: true },
-  { name: "Cadaqués", slug: "cadaques", province: "Girona", aliases: [], hasShield: false },
-  { name: "Calaf", slug: "calaf", province: "Barcelona", aliases: [], hasShield: false },
-  { name: "Calafell", slug: "calafell", province: "Tarragona", aliases: [], hasShield: false },
-  { name: "Calders", slug: "calders", province: "Barcelona", aliases: [], hasShield: false },
+  { name: "Cadaqués", slug: "cadaques", province: "Girona", aliases: [], hasShield: true },
+  { name: "Calaf", slug: "calaf", province: "Barcelona", aliases: [], hasShield: true },
+  { name: "Calafell", slug: "calafell", province: "Tarragona", aliases: [], hasShield: true },
+  { name: "Calders", slug: "calders", province: "Barcelona", aliases: [], hasShield: true },
   { name: "Caldes de Malavella", slug: "caldes-de-malavella", province: "Girona", aliases: [], hasShield: false },
   { name: "Caldes de Montbui", slug: "caldes-de-montbui", province: "Barcelona", aliases: [], hasShield: false },
   { name: "Caldes d'Estrac", slug: "caldes-destrac", province: "Barcelona", aliases: [], hasShield: false },
